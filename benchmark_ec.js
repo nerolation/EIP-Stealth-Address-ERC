c35f009eee09b939c15f0e1988108410d3e43ef5
Matt Solomon <matt@mattsolomon.dev>
2023-01-06 09:53:32 -0800

    remove unnecessary loops

--- benchmark_ec.js
+++ benchmark_ec.js
@@ -15,72 +15,60 @@ const P2 = secp.getPublicKey(p_scan);
 const numRuns = 1000;
 
 
+let startTime, endTime, deltaTime;
 console.log("elliptic.js:");
-for (let j = 0; j < 1; j++) {
-  const startTime = performance.now()
-  for (let k = 0; k < numRuns; k++) {
-    const P_scan = generatorPoint.mul(p_scan);
-  }
-  const endTime = performance.now()
-  const deltaTime = endTime - startTime;
-  console.log(`1000x privKey --> pubKey: ${deltaTime} milliseconds`);
+
+startTime = performance.now()
+for (let k = 0; k < numRuns; k++) {
+  const P_scan = generatorPoint.mul(p_scan);
 }
+endTime = performance.now()
+deltaTime = endTime - startTime;
+console.log(`1000x privKey --> pubKey: ${deltaTime} milliseconds`);
 
 
-for (let j = 0; j < 1; j++) {
-  const startTime = performance.now()
-  for (let k = 0; k < numRuns; k++) {
-    const P_scan =  P1.mul(p_scan);
-  }
-  const endTime = performance.now()
-  const deltaTime = endTime - startTime;
-  console.log(`1000x privKey * pubKey: ${deltaTime} milliseconds`);
+startTime = performance.now()
+for (let k = 0; k < numRuns; k++) {
+  const P_scan =  P1.mul(p_scan);
 }
+endTime = performance.now()
+deltaTime = endTime - startTime;
+console.log(`1000x privKey * pubKey: ${deltaTime} milliseconds`);
 
 
-
-for (let j = 0; j < 1; j++) {
-  const startTime = performance.now()
-  for (let k = 0; k < numRuns; k++) {
-    const P_scan = P1.add(P1);
-  }
-  const endTime = performance.now()
-  const deltaTime = endTime - startTime;
-  console.log(`1000x pubKey + pubKey: ${deltaTime} milliseconds`);
+startTime = performance.now()
+for (let k = 0; k < numRuns; k++) {
+  const P_scan = P1.add(P1);
 }
+endTime = performance.now()
+deltaTime = endTime - startTime;
+console.log(`1000x pubKey + pubKey: ${deltaTime} milliseconds`);
+
 console.log("-----------------------------------------");
 
 
 console.log("noble-secp256k1.js:");
-for (let j = 0; j < 1; j++) {
-  const startTime = performance.now()
-  for (let k = 0; k < numRuns; k++) {
-    const P_scan =  secp.getPublicKey(p_scan);
-  }
-  const endTime = performance.now()
-  const deltaTime = endTime - startTime;
-  console.log(`1000x privKey --> pubKey: ${deltaTime} milliseconds`);
+startTime = performance.now()
+for (let k = 0; k < numRuns; k++) {
+  const P_scan =  secp.getPublicKey(p_scan);
 }
+endTime = performance.now()
+deltaTime = endTime - startTime;
+console.log(`1000x privKey --> pubKey: ${deltaTime} milliseconds`);
 
 
-
-for (let j = 0; j < 1; j++) {
-  const startTime = performance.now()
-  for (let k = 0; k < numRuns; k++) {
-    const P_scan = secp.getSharedSecret(Buffer.from(p_scan, 'hex'), P2);
-  }
-  const endTime = performance.now()
-  const deltaTime = endTime - startTime;
-  console.log(`1000x privKey * pubKey: ${deltaTime} milliseconds`);
+startTime = performance.now()
+for (let k = 0; k < numRuns; k++) {
+  const P_scan = secp.getSharedSecret(Buffer.from(p_scan, 'hex'), P2);
 }
+endTime = performance.now()
+deltaTime = endTime - startTime;
+console.log(`1000x privKey * pubKey: ${deltaTime} milliseconds`);
 
-
-for (let j = 0; j < 1; j++) {
-  const startTime = performance.now()
-  for (let k = 0; k < numRuns; k++) {
-    const P_scan = secp.Point.fromHex(P2).add(secp.Point.fromHex(P2));
-  }
-  const endTime = performance.now()
-  const deltaTime = endTime - startTime;
-  console.log(`1000x pubKey + pubKey: ${deltaTime} milliseconds`);
+startTime = performance.now()
+for (let k = 0; k < numRuns; k++) {
+  const P_scan = secp.Point.fromHex(P2).add(secp.Point.fromHex(P2));
 }
+endTime = performance.now()
+deltaTime = endTime - startTime;
+console.log(`1000x pubKey + pubKey: ${deltaTime} milliseconds`);
