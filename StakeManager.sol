7834bd4b7c7aa77dd2fcc11dc2549ccdbf9ebdb0
Toni Wahrstätter <51536394+nerolation@users.noreply.github.com>
2023-04-02 21:20:52 +0200

    allow to specify staker in addStake function

--- StakeManager.sol
+++ StakeManager.sol
@@ -11,31 +11,30 @@ import "./IStakeManager.sol";
  */
 abstract contract StakeManager is IStakeManager {
 
-    /// maps paymaster to their deposits and stakes
+    /// maps paymaster to their stakes
     mapping(address => uint256) public stakes;
 
-    /// return the deposit of the account
+    /// return the stake of the account
     function balanceOf(address account) public view returns (uint256) {
         return stakes[account];
     }
 
     receive() external payable {
-        addStake();
+        addStake(msg.sender);
     }
 
     /**
-     * add to the account's stake - amount and delay
-     * any pending unstake is first cancelled.
+     * add to the account's stake - amount 
+     * @param staker the address of the staker.
      */
-    function addStake() public payable {
-        require(msg.value > 0, "no value specified");
-        stakes[msg.sender] += msg.value;
-        emit StakeDeposit(msg.sender, msg.value);
+    function addStake(address staker) public payable {
+        require(msg.value > 0, "invalid amount");
+        stakes[staker] += msg.value;
+        emit StakeDeposit(staker, msg.value);
     }
 
     /**
-     * withdraw from the (unlocked) stake.
-     * must first call unlockStake and wait for the unstakeDelay to pass
+     * withdraw the stake.
      * @param withdrawAddress the address to send withdrawn value.
      */
     function withdrawStake(address payable withdrawAddress) external {
